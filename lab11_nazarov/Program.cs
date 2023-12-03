ea93db49743cf72fcf568abda3c123760b7fe8b3
Nazarov Ruslan <rnazarov142@gmail.com>
2023-12-03 12:30:54 +0200

    Добавьте файлы проекта.

---
+++ lab11_nazarov/Program.cs
@@ -0,0 +1,31 @@
+﻿//Высокий уровень, Вариант 8 (Назаров Руслан)
+
+try
+{
+    Console.Write("Введите массив из 9 элементов (восьмеричные числа): ");
+    string[] octalArray = Console.ReadLine().Split(' ');
+
+    if (octalArray.Length != 9)
+    {
+        Console.WriteLine("Упс! Нужно ввести 9 элементов :) Сегодня без Random, нужно почувствовать программу, поэтому пишем вручную, пасиба :)");
+        return;
+    }
+
+    int[] decimalArray = new int[9];
+
+    for (int i = 0; i < 9; i++)
+    {
+        decimalArray[i] = Convert.ToInt32(octalArray[i], 8);
+    }
+
+    Console.WriteLine("Исходный массив в десятичной системе счисления:");
+    foreach (int number in decimalArray)
+    {
+        Console.Write($"{number} ");
+    }
+    Console.WriteLine(" <= вот ваши числа переведенные в десятичную систему счисления, удачи :)");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
